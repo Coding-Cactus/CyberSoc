db23247f6d7e2fc5011ab226c9949efefafdaa63
Coding-Cactus <codingcactus.cc@gmail.com>
2022-09-21 21:41:11 +0100

    :sparkles: Add backend

--- main.rb
+++ main.rb
@@ -1,7 +1,28 @@
-require "sinatra"
+# frozen_string_literal: true
 
-set :bind, "0.0.0.0"
+require 'sinatra'
+require_relative 'lib/cybersoc'
+
+set :bind, '0.0.0.0'
+
+db = CyberSoc::DataBase.new
+time_handler = CyberSoc::TimeHandler.new
 
 get '/' do
+  @dates = time_handler.dates.clone
+
+  db.all_talks(after: time_handler.start_of_day(Time.now)).each do |t|
+    start = time_handler.start_of_day(t.date)
+    @dates[start] = t if @dates.include?(start)
+  end
+
+  @msg = 'You entered something incorrectly' if params.include?('i')
+
   erb :index
+end
+
+post '/talk/create' do
+  redirect(
+    "/#{db.add_talk(params[:title], params[:author], Time.at(params[:date].to_i), params[:email]) ? '' : '?i'}"
+  )
 end
